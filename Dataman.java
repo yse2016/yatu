04b9cc3d146212e8a21733b889424cd9f95992d0
櫻井 優花 <160002@yses.ac.jp>
2017-07-04 11:24:36 +0900

    うーん

---
+++ Dataman.java
@@ -0,0 +1,9 @@
+   class Dataman{
+       // data
+       private String name;
+       
+       // method
+       public void hello(){
+         System.out.println("こんにちは！");
+       }
+     }
---
+++ TestData.java
@@ -0,0 +1,9 @@
+   public class TestData{
+       public static void main(String[] args){
+         // Datamanを新規作成する
+         Dataman dm = new Dataman();
+         
+         // Datamanの動作確認をする
+         dm.hello();
+       }
+     }
